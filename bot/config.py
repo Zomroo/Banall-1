cf2428c63ecad54b0093ab6e412e709555948ab6
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-15 22:35:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
-    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    TELEGRAM_TOKEN=os.environ['5619054777:AAF_XsEHxhJ7aXRRKowWzCR6R2u3vC1Hsi8']
+    TELEGRAM_APP_HASH=os.environ['b8105dc4c17419dfd4165ecf1d0bc100']
+    TELEGRAM_APP_ID=int(os.environ['15849735'])
     
     if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
